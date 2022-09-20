e8f9e0d43a9e6303234b8e58759a99eb25731f26
Edwinjose1 <113964710+Edwinjose1@users.noreply.github.com>
2022-09-20 10:51:37 +0530

    Add files via upload

---
+++ Responsive/pepsi.js
@@ -0,0 +1,23 @@
+function blue(){
+    document.getElementById("blue").src="https://www.pepsi.com/en-us/uploads/images/can-pepsi.png?mtime=20180110134757"
+    document.getElementById("shape1").style.backgroundColor="#004b93";
+    document.getElementById("learnmore").style.backgroundColor="#004b93";
+    document.getElementById("pepsi").style.color="#004b93";
+    document.getElementById("headbg").style.backgroundColor="#004b93";
+}
+function silver(){
+    document.getElementById("blue").src="https://www.pepsi.com/en-us/uploads/images/Diet-Pepsi-v2_12oz.png?mtime=20180215173735"
+    document.getElementById("shape1").style.backgroundColor="#c3c7c7";
+    document.getElementById("learnmore").style.backgroundColor="#c3c7c7";
+    document.getElementById("pepsi").style.color="#c3c7c7";
+    document.getElementById("headbg").style.backgroundColor="#c3c7c7";
+}
+function black(){
+    document.getElementById("blue").src="https://www.pepsi.com/en-us/uploads/images/can-pepsi-zero-sugar.png"
+    document.getElementById("shape1").style.backgroundColor="black";
+    document.getElementById("learnmore").style.backgroundColor="black";
+    document.getElementById("pepsi").style.color="black";
+    document.getElementById("headbg").style.backgroundColor="black";
+    
+
+}
